167c951e4ffb21839e9528f58eedc7ec44b650b2
Jim Carson <jim.carson@gmail.com>
2017-07-09 13:44:03 -0700

    Try to be more OS independent on location.  I don't have a Linux box to test on.

--- .jtmapper.R
+++ .jtmapper.R
@@ -2,10 +2,38 @@
 mycall       <- "WG1V"    # *Your* station callsign
 mygrid       <- "FN42fk"  # *Your* station location
 
-# WSJT-X log file.
-loglocation  <- file.path(Sys.getenv("HOME"),"/Library/Application Support/WSJT-X/ALL.TXT")
+
 # How many recent log lines to plot
 wsjtxlines   <- "50"
+
+# WSJT-X log file.
+# Attempt to be OS independent.  
+# NOTE: I am guessing at what the Linux path is.
+
+# Get the operating system.
+os = Sys.info()[['sysname']]
+
+if (os == "Darwin") {
+   wsjtpath = { file.path(path.expand("~"),"Library/Application Support/WSJT-X") }
+} else if (os == "Windows") { 
+   # deal with backslashes in environment variables.  Not sure how to map system
+   # drive, so this will fail for people who install on other drives.
+   wsjtpath = { 
+    file.path(
+      chartr("\\","/",
+        file.path(# Sys.getenv("HOMEDRIVE"),
+		  Sys.getenv("HOMEPATH"),"AppData/Local/WSJT-X")
+      )
+    ) 
+   }
+   # wsjtpath = { file.path(chartr("\\","/",path.expand("~")),"AppData/Local/WSJT-X") }
+
+   # On my virtual machine, HOME is set to \\Mac\Home\Documents; however, on a true 
+} else  { # linux, guessing.
+   wsjtpath = { file.path(path.expand("~"),"wsjt-x") } 
+}
+loglocation = file.path(wsjtpath,"ALL.TXT")
+
 
 # Colors
 cqcolor      <- "green4"
