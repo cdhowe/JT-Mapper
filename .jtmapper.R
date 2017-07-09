1edf84371d26ba15b2fb1c438d52a7ee04f210ba
Jim Carson <jim.carson@gmail.com>
2017-07-09 14:09:26 -0700

    Moved log file stuff into main program.

--- .jtmapper.R
+++ .jtmapper.R
@@ -6,34 +6,6 @@ mygrid       <- "FN42fk"  # *Your* station location
 # How many recent log lines to plot
 wsjtxlines   <- "50"
 
-# WSJT-X log file.
-# Attempt to be OS independent.  
-# NOTE: I am guessing at what the Linux path is.
-
-# Get the operating system.
-os = Sys.info()[['sysname']]
-
-if (os == "Darwin") {
-   wsjtpath = { file.path(path.expand("~"),"Library/Application Support/WSJT-X") }
-} else if (os == "Windows") { 
-   # deal with backslashes in environment variables.  Not sure how to map system
-   # drive, so this will fail for people who install on other drives.
-   wsjtpath = { 
-    file.path(
-      chartr("\\","/",
-        file.path(# Sys.getenv("HOMEDRIVE"),
-		  Sys.getenv("HOMEPATH"),"AppData/Local/WSJT-X")
-      )
-    ) 
-   }
-   # wsjtpath = { file.path(chartr("\\","/",path.expand("~")),"AppData/Local/WSJT-X") }
-
-   # On my virtual machine, HOME is set to \\Mac\Home\Documents; however, on a true 
-} else  { # linux, guessing.
-   wsjtpath = { file.path(path.expand("~"),"wsjt-x") } 
-}
-loglocation = file.path(wsjtpath,"ALL.TXT")
-
 
 # Colors
 cqcolor      <- "green4"
